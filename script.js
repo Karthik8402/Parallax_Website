4be19ba7c99c2d1fd3fed8a4cea4f37a3e8aba97
Karthik <124026006+Karthik8402@users.noreply.github.com>
2023-05-30 10:11:59 +0530

    Only view in desktop view only

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ window.addEventListener('scroll', () => {
     hill1.style.top = value * .8 + 'px';
 
 })
+ // Check if the user is in desktop mode
+ let isDesktopMode = window.matchMedia("(min-width: 1250px)").matches;
+      
+ // Function to handle content display
+ function handleContentDisplay() {
+   if (isDesktopMode) {
+     // Display the content for desktop mode
+     document.getElementById("content").style.display = "block";
+   } else {
+     // Hide the content for mobile mode
+     document.getElementById("content").style.display = "none";
+     window.alert("Your window size is less than 1250 resolution, So Turn to Desktop Mode")
+   }
+ }
+
+ // Call the function on page load and whenever the window is resized
+ window.addEventListener("load", handleContentDisplay);
+ window.addEventListener("resize", handleContentDisplay);
